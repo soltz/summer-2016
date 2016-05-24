c2e2396c70fba6fb97abd925e2c087587774a584
Simpson <simpson48@ulani.llnl.gov>
2016-05-24 11:29:25 -0700

    readding 3d jet plot for hard processes

--- pydir/3d_jetplot_2016.py
+++ pydir/3d_jetplot_2016.py
@@ -26,7 +26,7 @@ phi = []
 pT = []
 eT = []
 
-
+# Used for position and width of bars
 base = []
 d_eta = []
 d_phi = []
@@ -38,25 +38,33 @@ pythia.readString("PhaseSpace:pTHatMin = 20.")
 pythia.init()
 
 # Begin event loop. Generate
-for iEvent in range(0,1):
+for iEvent in range(0,10000):
     if not pythia.next(): break
 
+    a = pythia.info.code()
     event_type = pythia.info.name()
 
-    for prt in pythia.event:
-        if prt.isFinal():
-            prt_eta = prt.eta()
-            prt_phi = prt.phi()
-            prt_pT = prt.pT()
-            prt_eT = prt.eT()
+    # choose a hard process to plot using the desired code
+    # 111:gg2gg, 112:gg2qqbar, 113:qg2qg, 114:qq(bar)'2qq(bar)', 115:qqbar2gg, 116:qqbar2q'qbar'
+    # 121:gg2ccbar, 122:qqbar2ccbar, 123:gg2bbbar, 124:qqbar2bbbar
+    if a == 112:
+        for prt in pythia.event:
+            if prt.isFinal():
+                prt_eta = prt.eta()
+                prt_phi = prt.phi()
+                prt_pT = prt.pT()
+                prt_eT = prt.eT()
 
-            eta.append(prt_eta)
-            phi.append(prt_phi)
-            pT.append(prt_pT)
-            eT.append(prt_eT)
-            base.append(0)
-            d_eta.append(0.1)
-            d_phi.append(0.1)
+                eta.append(prt_eta)
+                phi.append(prt_phi)
+                pT.append(prt_pT)
+                eT.append(prt_eT)
+
+                # base should remain set at 0, d_eta and d_phi adjust bar thickness
+                base.append(0)
+                d_eta.append(0.1)
+                d_phi.append(0.1)
+        break
 
  
 # End of event loop. Statistics. Histogram. Done.
@@ -68,7 +76,7 @@ ax.bar3d(eta,phi,base,d_eta,d_phi,eT)
 
 ax.set_xlabel('$\eta$')
 ax.set_ylabel('$\phi$')
-ax.set_zlabel('eT')
+ax.set_zlabel('eT(GeV)')
 
 title = 'hard process: ' + event_type
 plt.title(title)
