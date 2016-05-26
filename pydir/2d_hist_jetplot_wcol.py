58f9da166e77b403ebf20f86b9827e3ace725675
Simpson <simpson48@ulani.llnl.gov>
2016-05-26 11:28:34 -0700

    Program with improved color

--- pydir/2d_hist_jetplot_wcol.py
+++ pydir/2d_hist_jetplot_wcol.py
@@ -74,7 +74,6 @@ def main():
     slowJet = pythia8.SlowJet( -1, radius, pTjetMin, etaMax, nSel, 1);
     
     while True:
-
 #   Empty lists to store data from jet particles        
         jet_eta = []
         jet_phi = []
@@ -87,6 +86,7 @@ def main():
         bg_eT = []
 
         if not pythia.next(): break
+        pythia.event.list()
 
         slowJet.analyze(pythia.event)
         slowJet.list()
@@ -119,6 +119,7 @@ def main():
 
 #   Create bins using np.histogram2d(), the number of bins produced is the square of the value "b"
         b = 20
+        tot_bins = b**2
         bg_h, xedges, yedges = np.histogram2d(bg_eta,bg_phi,bins=b,weights=bg_eT)
         bg_h = np.concatenate(bg_h)
         xcenters = 0.5*(xedges[1:]+xedges[:-1])
@@ -130,47 +131,54 @@ def main():
         jet_h, jet_xedges, jet_yedges = np.histogram2d(jet_eta,jet_phi,bins=[xedges,yedges],weights=jet_eT)
         jet_h = np.concatenate(jet_h)
 
-        bg_h = list(bg_h)
-        jet_h = list(jet_h)
-        bg_h.extend(jet_h)
-        h = bg_h
-
         xcenters = list(xcenters)
         ycenters = list(ycenters)
         xcenters.extend(xcenters)
         ycenters.extend(ycenters)
         
 #   Bars associated with jets will sit on top of bars associated with background        
-        bg_base = np.zeros(len(bg_h))
+        bg_base = np.zeros(len(jet_h))
         bg_base = list(bg_base)
         jet_base = bg_h
         bg_base.extend(jet_base)
         base = bg_base
         
+        bg_h = list(bg_h)
+        jet_h = list(jet_h)
+        bg_h.extend(jet_h)
+        h = bg_h
+        
 #   Set size of bars in plot
-        d_eta = xedges[1]-xedges[0]
-        d_phi = yedges[1]-yedges[0]
+        d_eta = (xedges[1]-xedges[0])
+        d_phi = (yedges[1]-yedges[0])
 
 #   Create an array of colors where bars associated with jet particles are red
+#   Background bars are blue and bars with no value are white
         jet_colors=[]
-        for i in range(len(jet_h)):
-            jet_colors.append('b')
-        for i in jet_h:
+        for i in h[0:tot_bins]:
             if i > 0:
-                jet_colors.append('r')
-            else:
                 jet_colors.append('b')
+            else:
+                jet_colors.append('w')
+        for i in range(len(jet_h)):
+            if jet_h[i] > 0:
+                jet_colors.append('r')
+            elif h[i] > 0:
+                jet_colors.append('b')
+            else:
+                jet_colors.append('w')
 
 #   End of event loop. Statistics. Histogram. Done.
         pythia.stat();
         
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.bar3d(xcenters,ycenters,base,d_eta,d_phi,h,color=jet_colors)
+        ax.bar3d(xcenters,ycenters,base,d_eta,d_phi,h,color=jet_colors,alpha=1)
 
-        ax.set_xlabel('$\eta$')
-        ax.set_ylabel('$\phi$')
+        ax.set_xlabel('\n$\eta$',fontsize=20)
+        ax.set_ylabel('\n$\phi$',fontsize=20)
         ax.set_zlabel('eT(GeV)')
+        ax.text(0,0,5,'TEST')
 
         event_type = pythia.info.name()
         title = 'hard process: ' + event_type
@@ -181,6 +189,11 @@ def main():
         
         pythia.settings.listChanged()
         pythia.settings.list("Random:seed")
+
+        for i in range(slowJet.sizeJet()):
+            print slowJet.pT(i)
+        print pythia.event[5].pT()
+        print pythia.event[6].pT()
 
         query = raw_input("q to quit, p to save to pdf,  <CR> to continue: ")
         if (query=='q'):
