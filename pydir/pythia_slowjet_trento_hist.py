bc9cf6507af8e33d22f69c77152ba9a01234cd78
Simpson <simpson48@ulani.llnl.gov>
2016-06-07 15:50:30 -0700

    added pythia trento slowjet file

--- pydir/pythia_slowjet_trento_hist.py
+++ pydir/pythia_slowjet_trento_hist.py
@@ -79,18 +79,31 @@ for i in range(len(mult)):
     phi = []
     eta = []
 
-    jet_eta = []
-    jet_phi = []
-    jet_pT = []
-    jet_eT = []
+#   empty lists for pythia particle info
+    pjet_eta = []
+    pjet_phi = []
+    pjet_pT = []
+    pjet_eT = []
         
-    bg_eta = []
-    bg_phi = []
-    bg_pT = []
-    bg_eT = []
-    
+    pbg_eta = []
+    pbg_phi = []
+    pbg_pT = []
+    pbg_eT = []
 
+#   empty lists for trento particle info
+    tjet_eta = []
+    tjet_phi = []
+    tjet_pT = []
+    tjet_eT = []
+        
+    tbg_eta = []
+    tbg_phi = []
+    tbg_pT = []
+    tbg_eT = []
+    
     pythia.event.reset()
+    pythia.next()
+    pythia.event.size()
     
     for j in range(mult[i]):
         r1, r2, r3, r4 = np.random.random(4)
@@ -142,8 +155,8 @@ for i in range(len(mult)):
         totJetPrtList = np.concatenate(slowJetPrtList)
     else: totJetPrtList = [0]
 
-#   Loop over pythia event particles
-    for j in range(pythia.event.size()):
+#   Loop over pythia particles
+    for j in range(pythia.event.size() - mult[i]):
         prt = pythia.event[j]
         if prt.isFinal():
             prt_eta = prt.eta()
@@ -151,46 +164,104 @@ for i in range(len(mult)):
             prt_pT = prt.pT()
             prt_eT = prt.eT()
             if j in totJetPrtList:
-                jet_eta.append(prt_eta)
-                jet_phi.append(prt_phi)
-                jet_pT.append(prt_pT)
-                jet_eT.append(prt_eT)
+                pjet_eta.append(prt_eta)
+                pjet_phi.append(prt_phi)
+                pjet_pT.append(prt_pT)
+                pjet_eT.append(prt_eT)
             else:
-                bg_eta.append(prt_eta)
-                bg_phi.append(prt_phi)
-                bg_pT.append(prt_pT)
-                bg_eT.append(prt_eT)
+                pbg_eta.append(prt_eta)
+                pbg_phi.append(prt_phi)
+                pbg_pT.append(prt_pT)
+                pbg_eT.append(prt_eT)
+
+#   Loop over trento particles
+    for j in range(pythia.event.size() - mult[i], pythia.event.size()):
+        prt = pythia.event[j]
+        if prt.isFinal():
+            prt_eta = prt.eta()
+            prt_phi = prt.phi()
+            prt_pT = prt.pT()
+            prt_eT = prt.eT()
+            if j in totJetPrtList:
+                tjet_eta.append(prt_eta)
+                tjet_phi.append(prt_phi)
+                tjet_pT.append(prt_pT)
+                tjet_eT.append(prt_eT)
+            else:
+                tbg_eta.append(prt_eta)
+                tbg_phi.append(prt_phi)
+                tbg_pT.append(prt_pT)
+                tbg_eT.append(prt_eT)
         
 #   Create bins using np.histogram2d(), the number of bins produced is the square of the value "b"
     b = 20
     tot_bins = b**2
-    bg_h, xedges, yedges = np.histogram2d(bg_eta,bg_phi,bins=b,range=[[-2,2], [-(np.pi), np.pi]],weights=bg_eT)
-    bg_h = np.concatenate(bg_h)
+
+#   Data from trento background is binned
+    tbg_h, xedges, yedges = np.histogram2d(tbg_eta,tbg_phi,bins=b,range=[[-2,2], [-(np.pi), np.pi]],weights=tbg_eT)
+    tbg_h = np.concatenate(tbg_h)
+
+#   Data from trento particles identified as jets is binned    
+    tjet_h, tjet_xedges, tjet_yedges = np.histogram2d(tjet_eta,tjet_phi,bins=[xedges,yedges],weights=tjet_eT)
+    tjet_h = np.concatenate(tjet_h)
+
+#   Data from pythia background is binned
+    pbg_h, pbg_xedges, pbg_yedges = np.histogram2d(pbg_eta,pbg_phi,bins=[xedges,yedges],weights=pbg_eT)
+    pbg_h = np.concatenate(pbg_h)
+
+#   Data from pythia particles identified as jets is binned
+    pjet_h, pjet_xedges, pjet_yedges = np.histogram2d(pjet_eta,pjet_phi,bins=[xedges,yedges],weights=pjet_eT)
+    pjet_h = np.concatenate(pjet_h)
+
+#   x and y coordinates for bincenters are created using bin edges
     xcenters = 0.5*(xedges[1:]+xedges[:-1])
     xcenters = np.repeat(xcenters,b)
     ycenters = 0.5*(yedges[1:]+yedges[:-1])
     ycenters = np.tile(ycenters,b)
 
-#   Data from jet particles is binned separately        
-    jet_h, jet_xedges, jet_yedges = np.histogram2d(jet_eta,jet_phi,bins=[xedges,yedges],weights=jet_eT)
-    jet_h = np.concatenate(jet_h)
-
+#   arrays of bin centers are extended to match the length of the data sets
     xcenters = list(xcenters)
     ycenters = list(ycenters)
     xcenters.extend(xcenters)
     ycenters.extend(ycenters)
+    xcenters.extend(xcenters)
+    ycenters.extend(ycenters)
         
-#   Bars associated with jets will sit on top of bars associated with background        
-    bg_base = np.zeros(len(jet_h))
-    bg_base = list(bg_base)
-    jet_base = bg_h
-    bg_base.extend(jet_base)
-    base = bg_base
+#   Bars associated with jets will sit on top of bars associated with background
+#   the order bars will be stacked is trento bg, pythia bg, trento jet, pythia jet     
+
+#   the base for trento bg will be all zeroes
+    tbg_base = np.zeros(len(tjet_h))
+    tbg_base = list(tbg_base)
+
+#   the base for pythia bg will be the top of the trento bg
+    pbg_base = tbg_h
+    pbg_base = list(pbg_base)
+    tbg_base.extend(pbg_base)
+    pbg_base = np.array(pbg_base)
+
+#   the base for trento jets will be the top of trento bg and pythia bg
+    tjet_base = pbg_base + pbg_h
+    tjet_base = list(tjet_base)
+    tbg_base.extend(tjet_base)
+    tjet_base = np.array(tjet_base)
+
+#   the base for pythia jets will be on top of trento bg, pythia bg, and trento jets
+    pjet_base = tjet_base + tjet_h
+    pjet_base = list(pjet_base)
+    tbg_base.extend(pjet_base)
+
+    base = tbg_base
         
-    bg_h = list(bg_h)
-    jet_h = list(jet_h)
-    bg_h.extend(jet_h)
-    h = bg_h
+    tbg_h = list(tbg_h)
+    pbg_h = list(pbg_h)
+    tjet_h = list(tjet_h)
+    pjet_h = list(pjet_h)
+    tbg_h.extend(pbg_h)
+    tbg_h.extend(tjet_h)
+    tbg_h.extend(pjet_h)
+
+    h = tbg_h
         
 #   Set size of bars in plot
     d_eta = (xedges[1]-xedges[0])
@@ -199,16 +270,42 @@ for i in range(len(mult)):
 #   Create an array of colors where bars associated with jet particles are red
 #   Background bars are blue and bars with no value are white
     jet_colors=[]
+    
+    tbg_col = 'skyblue'
+    pbg_col = 'blue'
+    tjet_col = 'yellow'
+    pjet_col = 'red'
+    
     for j in h[0:tot_bins]:
         if j > 0:
-            jet_colors.append('b')
+            jet_colors.append(tbg_col)
         else:
             jet_colors.append('w')
-    for j in range(len(jet_h)):
-        if jet_h[j] > 0:
-            jet_colors.append('r')
+    for j in range(len(pbg_h)):
+        if pbg_h[j] > 0:
+            jet_colors.append(pbg_col)
         elif h[j] > 0:
-            jet_colors.append('b')
+            jet_colors.append(tbg_col)
+        else:
+            jet_colors.append('w')
+    for j in range(len(tjet_h)):
+        if tjet_h[j] > 0:
+            jet_colors.append(tjet_col)
+        elif pbg_h[j] > 0:
+            jet_colors.append(pbg_col)
+        elif h[j] > 0:
+            jet_colors.append(tbg_col)
+        else:
+            jet_colors.append('w')
+    for j in range(len(pjet_h)):
+        if pjet_h[j] > 0:
+            jet_colors.append(pjet_col)
+        elif tjet_h[j] > 0:
+            jet_colors.append(tjet_col)
+        elif pbg_h[j] > 0:
+            jet_colors.append(pbg_col)
+        elif h[j] > 0:
+            jet_colors.append(tbg_col)
         else:
             jet_colors.append('w')
 
@@ -225,10 +322,10 @@ for i in range(len(mult)):
     pi = np.pi
     plt.ylim(-pi,pi)
 
-#    for j in range(slowJet.sizeJet()):
-#        pT_jet = slowJet.pT(j)
+#    for i in range(slowJet.sizeJet()):
+#        pT_jet = slowJet.pT(i)
 #        pT_label = 'slowJet pT = %.2f' % pT_jet
-#        ax.text(slowJet.y(j),slowJet.phi(j),max(h)+0.1,pT_label,horizontalalignment='center')
+#        ax.text(slowJet.y(i),slowJet.phi(i),max(h)+0.1,pT_label,horizontalalignment='center')
 
     plt.show(block = False)
 
@@ -250,4 +347,3 @@ for i in range(len(mult)):
     else: continue
 
     
-
